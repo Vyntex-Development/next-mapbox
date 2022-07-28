b83594ef71648c391360acc76ffa5ca2a243a948
adamovic6666 <adamovicslobodan@yahoo.com>
2022-07-28 15:13:10 +0200

    city change returned to 6 months

--- src/components/UI/SlideModal.js
+++ src/components/UI/SlideModal.js
@@ -82,7 +82,7 @@ const SildeModal = ({
 
   const changeAddressHandler = async () => {
     //262974
-    let sixMonths = 1;
+    let sixMonths = 262974;
     if (minutesDiff < sixMonths) {
       setError(true);
       return;
